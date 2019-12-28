41d0eb2abe6069116e044ad401d912089deba816
Emanuel Rabina <emanuelrabina@gmail.com>
2019-12-29 12:35:51 +1300

    Test time is logged

---
+++ tests/nz/net/ultraq/extensions/ProfilingExtensionsTests.groovy
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.extensions
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+
+/**
+ * Tests for the profiling extension methods.
+ * 
+ * @author Emanuel Rabina
+ */
+class ProfilingExtensionsTests extends Specification {
+
+	def "Logs the time the closure took to execute"() {
+		given:
+			def mockLogger = Mock(Logger)
+			GroovyStub(LoggerFactory, global: true).getLogger(_) >> mockLogger
+		when:
+			ProfilingExtensions.time(null, 'Test') {
+				// Nothing happening here
+			}
+		then:
+			1 * mockLogger.debug({ it ==~ /Test complete\.  Execution time: [\d]+ms/ })
+	}
+}
